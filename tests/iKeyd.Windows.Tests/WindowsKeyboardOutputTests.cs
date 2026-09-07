23bb51f7c2b664aa07c036e36e7a79d79503b128
SLktEx <85579720+SLktEx@users.noreply.github.com>
2026-09-07 12:37:37 +0900

    Test #173 physical number and function-row replay

--- tests/iKeyd.Windows.Tests/WindowsKeyboardOutputTests.cs
+++ tests/iKeyd.Windows.Tests/WindowsKeyboardOutputTests.cs
@@ -24,6 +24,36 @@ public sealed class WindowsKeyboardOutputTests
         Assert.NotEqual(0u, input.Data.Keyboard.Flags & 0x0008u);
     }
 
+    [Theory]
+    [InlineData((ushort)'1', (ushort)0x02)]
+    [InlineData((ushort)'9', (ushort)0x0A)]
+    [InlineData((ushort)'0', (ushort)0x0B)]
+    [InlineData((ushort)0x70, (ushort)0x3B)]
+    [InlineData((ushort)0x79, (ushort)0x44)]
+    [InlineData((ushort)0x7A, (ushort)0x57)]
+    [InlineData((ushort)0x7B, (ushort)0x58)]
+    public void Number_and_function_identity_replay_uses_physical_scan_code(ushort virtualKey, ushort expectedScanCode)
+    {
+        var normalized = WindowsKeyboardOutput.NormalizeIdentityReplayKey(new KeyboardKey(virtualKey, 0));
+        var input = WindowsKeyboardOutput.BuildKeyInput(normalized, KeyEventKind.Down);
+
+        Assert.Equal(new KeyboardKey(0, expectedScanCode), normalized);
+        Assert.Equal((ushort)0, input.Data.Keyboard.VirtualKey);
+        Assert.Equal(expectedScanCode, input.Data.Keyboard.ScanCode);
+        Assert.NotEqual(0u, input.Data.Keyboard.Flags & 0x0008u);
+        Assert.Equal(WindowsKeyboardOutput.InjectionMarker, input.Data.Keyboard.ExtraInfo);
+    }
+
+    [Fact]
+    public void Identity_replay_normalization_does_not_change_new_shita_letter_output_or_explicit_scan_input()
+    {
+        var letter = new KeyboardKey((ushort)'F', 0);
+        var explicitPhysical = new KeyboardKey((ushort)'1', 0x02);
+
+        Assert.Equal(letter, WindowsKeyboardOutput.NormalizeIdentityReplayKey(letter));
+        Assert.Equal(explicitPhysical, WindowsKeyboardOutput.NormalizeIdentityReplayKey(explicitPhysical));
+    }
+
     [Fact]
     public void Combined_vk_sc_legacy_key_uses_the_pair_preserving_compatibility_path()
     {
